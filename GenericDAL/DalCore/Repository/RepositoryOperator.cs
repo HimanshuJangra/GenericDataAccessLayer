1f27f60d4c51945effab076a4c921c1d24bbaa42
Roman Kern <mkdprojekt@gmail.com>
2016-05-20 16:33:02 +0200

    Proxy

---
+++ GenericDAL/DalCore/Repository/RepositoryOperator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalCore.Repository
+{
+    public static class RepositoryOperator
+    {
+    }
+}
